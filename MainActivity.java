94acb7e3b3ede85195b7e13f7a3c7ea2ad73eea3
kanwarwalia <kanwarwalia66@gmail.com>
2019-02-05 22:41:08 -0500

    abc2

--- MainActivity.java
+++ MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSystemServiceName(0).indexOf(0);
     }
 }
